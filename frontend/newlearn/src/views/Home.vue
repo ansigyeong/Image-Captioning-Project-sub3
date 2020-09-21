9476b8ba5ef884346b5fceb56d4e36231dcce20a
안시경 <jeongseok95@naver.com>
2020-09-21 20:17:22 +0900

    S03P23D107-25 | 홈 화면 수정

--- frontend/newlearn/src/views/Home.vue
+++ frontend/newlearn/src/views/Home.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="home">
-    <LoginView v-if="!isLoggedIn" />
+    <img src="@/assets/Newyork.jpg" alt="" style="width: 100%;">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-import LoginView from '../views/accounts/LoginView.vue'
-import { mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
-  computed: {
-    ...mapGetters(['isLoggedIn'])
-  },
   components: {
-    LoginView,
   }
 }
 </script>
 
 <style socped>
-  body {
-    background-image: url('../assets/Newyork.jpg');
-    background-size: 100%;
-  }
+
 </style>
