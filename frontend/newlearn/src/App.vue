3b843a372ef64f421b2c6ff3b2bee03bb94a37ce
안시경 <jeongseok95@naver.com>
2020-09-23 19:40:01 +0900

    Speaking 링크
    Former-commit-id: 8a65a0d9fe4b354e7e9390feb3ade5e8f8b3ced4 [formerly 2aef0e35193d352f623f69055f530a634229539a]
    Former-commit-id: 7ee5dd1bd309d4367c2677be2f8370e90c1ca15a

--- frontend/newlearn/src/App.vue
+++ frontend/newlearn/src/App.vue
@@ -80,7 +80,7 @@
                     <v-list-item-title>Listening</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
-            <v-list-item>
+            <v-list-item @click="goSpeaking">
                 <v-list-item-icon>
                     <i class="fas fa-volume-up"></i>
                 </v-list-item-icon>
@@ -200,6 +200,9 @@ export default {
       goWordbook() {
           this.$router.push('/english/wordbook')
       },
+      goSpeaking() {
+          this.$router.push('/english/speaking')
+      }
       
     },
     mounted() {
