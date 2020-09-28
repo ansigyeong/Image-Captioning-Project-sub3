efd1ec26ac02dc7728db194f23872bc98a596a40
안시경 <jeongseok95@naver.com>
2020-09-28 20:30:13 +0900

    S03P23D107-38 | 공지사항 detail
    Former-commit-id: d3985adf12ab0200dd5b0243fb6eaadb6f821d06 [formerly 1e66f50959f606c7bd6f8bb5f6a9db067ebef829]
    Former-commit-id: 95b3f220ab83dd0241d3a8abe673b9adb784f5cb

--- frontend/newlearn/src/views/noticeDetail.vue
+++ frontend/newlearn/src/views/noticeDetail.vue
@@ -1,5 +1,29 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div style="text-align: center;"><h1>🔉 Notice 🔉</h1></div>
+    <br>
+    <br>
+
+    <!-- title & etc -->
+    <div class="title">
+      <h1>{{ this.notice.title }}</h1>
+    </div>
+    <div style="text-align: right;">
+      <p>{{ this.notice.created_at }}</p>
+    </div>
+    <div style="text-align: right;">
+      <v-btn>EDIT</v-btn>
+      <!-- <v-btn @click="goDelete">DELETE</v-btn> -->
+    </div>
+
+    <hr>
+
+    <!-- content -->
+    <div v-html="this.notice.content" style="margin:20px" class="contentbox"></div>
+    <!-- <div class="content">
+      <h3>{{ this.suggestion.content }}</h3>
+    </div> -->
+  </div>
 </template>
 
 <script>
@@ -23,7 +47,7 @@ export default {
           Authorization: `Token ${this.$cookies.get('auth-token')}`
         }
       }
-      http.get("/notice/noticedetail/" + this.notice_pk, config)
+      http.get("/community/notice/" + this.notice_pk, config)
       .then((res) => {
         this.notice = res.data
       })
@@ -34,5 +58,13 @@ export default {
 </script>
 
 <style scoped>
-
+  .title {
+    margin: 20px;
+  }
+  .content {
+    margin: 20px;
+  }
+  .v-btn {
+    margin: 5px;
+  }
 </style>
