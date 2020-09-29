96c5b49646e00f5786c2ed9ce64b0e6d0d377b84
Lunarus00 <moongur1900@naver.com>
2020-09-29 17:17:09 +0900

    S03P23D107-37 | 공지사항 작성 권한 운영자에게만 부여
    Former-commit-id: ead275118796c5d646fc29862162a7e1821c9428 [formerly 3c9e6782e7b8530bf95261fe3e8bf146774fbda8]
    Former-commit-id: d0992bdaa6cf2fb10eaab05a383ac97ac5731572

--- backend/AI/community/urls.py
+++ backend/AI/community/urls.py
@@ -5,6 +5,7 @@ app_name = 'community'
 
 urlpatterns = [
     path('notice/', views.notice_list),
+    path('notice/check/', views.noticecheck),
     path('notice/create/', views.notice_create),
     path('notice/<int:notice_pk>/', views.notice_detail),
     path('notice/<int:notice_pk>/delete/', views.notice_delete),
