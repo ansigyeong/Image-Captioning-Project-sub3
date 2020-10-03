cb676450e185d5026599382611113ba7c11c7adf
Daeheum <conquerer1209@gmail.com>
2020-10-03 22:45:33 +0900

    css
    Former-commit-id: 9d0ff0f522674ced6429325c7b7bcb0f726ecaa2 [formerly f719a215a3ed5fc7b24cb72892fc4fc8d2fed496]
    Former-commit-id: 8e8c5d797570b33e224823528cc46d95e2275478

--- frontend/newlearn/src/views/accounts/SignupView.vue
+++ frontend/newlearn/src/views/accounts/SignupView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="text-align: center; vertical-align: middle;">
-      <a href="/"><h1 style="font-size: 100px; color: white;">New Learn</h1></a>
+      <h1 style="font-size: 100px; color: white;">New Learn</h1>
     </div>
     <br>
     <br>
