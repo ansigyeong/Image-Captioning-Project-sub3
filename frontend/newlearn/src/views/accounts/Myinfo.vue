7b9539f76ece8ed0e97ce740d9e4e6a49ac036ab
Lunarus00 <moongur1900@naver.com>
2020-10-03 19:40:27 +0900

    내 정보 수정 프로필 이미지 삭제
    Former-commit-id: c49af4829f065ef90c87cadc702cd114e98aa47b [formerly 3f46a77bbcd578d410a84b04f80dabc85b6b7020]
    Former-commit-id: bd3d5e7f67cb3eafc096c2a4223fd25b60837b91

--- frontend/newlearn/src/views/accounts/Myinfo.vue
+++ frontend/newlearn/src/views/accounts/Myinfo.vue
@@ -10,25 +10,6 @@
       <br>
 
       <!-- 1. 프로필 카드 -->
-      <v-card class="mx-auto" max-width="434" tile>
-        <v-img height="100%" src="@/assets/profile.jpg">
-          <v-row align="end" class="fill-height">
-            <v-col align-self="start" class="pa-0" cols="12">
-              <!-- <v-avatar class="profile" color="grey" size="164" tile>
-                <v-img src="@/assets/profile.jpg"></v-img>
-              </v-avatar> -->
-            </v-col>
-            <v-col class="py-0">
-              <v-list-item color="rgba(0, 0, 0, .4)" dark>
-                <v-list-item-content>
-                  <!-- <v-list-item-title class="title">{{ }}</v-list-item-title> -->
-                  <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-col>
-          </v-row>
-        </v-img>
-      </v-card>
 
       <br>
       <br>
