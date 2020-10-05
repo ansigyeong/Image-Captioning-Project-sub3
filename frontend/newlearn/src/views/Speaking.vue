e15bc4103e6080917426859851fb7e8a3ffe016a
Lunarus00 <moongur1900@naver.com>
2020-10-05 15:53:44 +0900

    필요없는 console.log 제거, 출석부 css 깨짐 수정
    Former-commit-id: a671ab265b534434e3a092e545eb2ea5c2b8cea9 [formerly 0ff318bd14a9ef90ed24ade98d2c67c22f3df96d]
    Former-commit-id: 0fffd8b1a795875b90a6d33e5325b9d4c984fdd6

--- frontend/newlearn/src/views/Speaking.vue
+++ frontend/newlearn/src/views/Speaking.vue
@@ -134,7 +134,7 @@ export default {
             .put(`/english/speaksound/`, InputData)
             .then((res) => {
                 this.userVoice = res.data
-                console.log(res.data)
+                // console.log(res.data)
             })
             .catch((err) => {
                 console.log(err)
@@ -143,16 +143,14 @@ export default {
         callRandomImage() {
             http.post(`/english/speaking/`)
             .then(res => {
-                console.log(res.data)
+                // console.log(res.data)
                 this.image = "http://localhost:8000" + res.data.serializer[0].image
                 this.capText = res.data.return_text
             })
         },
         checkCaption() {
             http.post(`/english/imagecaption/`)
-            .then(res => {
-                console.log(res.data)
-            })
+            .then()
         },
         imageUpload() {
             this.previewImg = {
@@ -185,7 +183,7 @@ export default {
             http
             .post(`/english/speakSituation/`)
             .then((res) => {
-                console.log(res)
+                // console.log(res)
                 this.userVoice = res.data
             })
         }
